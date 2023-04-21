6d2d4c551d5af86956103b031c4f44467dce396f
unknown <oleksiypetrochenko@gmail.com>
2023-04-21 17:21:00 +0300

    dont truncate admins

--- app/Console/Commands/CreateRootAdmin.php
+++ app/Console/Commands/CreateRootAdmin.php
@@ -34,8 +34,6 @@ class CreateRootAdmin extends Command
      */
     public function handle(RoleRepository $roleRepository, AdministratorRepository $administratorRepository, UserRepository $userRepository)
     {
-        DB::table('administrators_to_moderators_pivot')->truncate();
-
         $user = $userRepository->findBy([
             'email' => env('ADMIN_EMAIL'),
         ]);
