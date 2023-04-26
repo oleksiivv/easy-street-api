a9a1d5669ca0a73e82b82ad77903c3dbe4cae2a8
unknown <oleksiypetrochenko@gmail.com>
2023-04-26 18:32:25 +0300

    Fix create admin command

--- app/Console/Commands/CreateRootAdmin.php
+++ app/Console/Commands/CreateRootAdmin.php
@@ -39,6 +39,7 @@ class CreateRootAdmin extends Command
         ]);
 
         $user->role_id = $roleRepository->findByName(Role::ROLE_ADMIN)->id;
+        $user->email_is_confirmed = true;
         $user->save();
 
         $administratorRepository->createOrUpdate($user->email, $user->email, $user->id);
