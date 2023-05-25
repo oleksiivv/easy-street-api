489e3af268b47a999d3cc79752a67489670ec4b7
unknown <oleksiypetrochenko@gmail.com>
2023-05-25 16:03:25 +0300

    Fix team member access

--- app/Services/CompanyAccessService.php
+++ app/Services/CompanyAccessService.php
@@ -21,14 +21,14 @@ class CompanyAccessService
 
         $user = $this->userRepository->get($userId);
 
-        $moderators = $company->moderators;
+        $teamMembers = $company->team_members;
 
         if (in_array([
             'first_name' => $user->first_name,
             'last_name' => $user->last_name,
             'email' => $user->email,
             'id' => $user->id,
-        ], $moderators)) {
+        ], $teamMembers)) {
             return false;
         }
 
--- app/Services/GameAccessService.php
+++ app/Services/GameAccessService.php
@@ -21,14 +21,14 @@ class GameAccessService
 
         $user = $this->userRepository->get($userId);
 
-        $moderators = $game->publisher->moderators;
+        $teamMembers = $game->publisher->team_members;
 
         if (in_array([
             'first_name' => $user->first_name,
             'last_name' => $user->last_name,
             'email' => $user->email,
             'id' => $user->id,
-        ], $moderators ?? [])) {
+        ], $teamMembers)) {
             return false;
         }
 
